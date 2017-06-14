b81cee25001ec14b80ea88dbe50ac2b662659a6d
student04 <smeesathit@gmail.com>
2017-06-14 09:30:31 +0700

    Initial view success	(bind object to view)

--- Easy Authen/ViewController.swift
+++ Easy Authen/ViewController.swift
@@ -9,7 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Create and bind object to view
+    @IBOutlet weak var userTextField: UIImageView!
 
+    @IBOutlet weak var passwordTextField: UIImageView!
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    // Create method for button action
+    @IBAction func LoginButton(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
